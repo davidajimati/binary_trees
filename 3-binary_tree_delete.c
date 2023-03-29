599b0078f6f6b22dd60dff5bb2a5bfc35a3bfcdc
davidajimati <ajimatidavid@gmail.com>
2023-03-29 14:58:54 +0100

    tree delete update

--- 3-binary_tree_delete.c
+++ 3-binary_tree_delete.c
@@ -8,6 +8,11 @@
 
 void binary_tree_delete(binary_tree_t *tree)
 {
-	if (tree)
-		tree = NULL;
+	if (tree == NULL)
+		return;
+
+	binary_tree_delete(tree->left);
+	binary_tree_delete(tree->right);
+
+	free(tree);
 }

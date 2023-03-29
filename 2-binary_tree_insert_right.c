0ebab942126418009d5abc3dac55e38816837c4c
davidajimati <ajimatidavid@gmail.com>
2023-03-29 14:34:44 +0100

    insert to new_node's right

--- 2-binary_tree_insert_right.c
+++ 2-binary_tree_insert_right.c
@@ -3,7 +3,7 @@
 /**
  * binary_tree_insert_left - inserts a node as the left-child of another node
  *
- * @parent: pointer to the node to insert the left-child
+ * @parent: pointer to the node to insert the right-child
  * @value:  value to store in the new node
  * Return: pointer to the new node or NULL if mem. allocation fails.
 */
@@ -19,16 +19,12 @@ binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value)
 
 	new_node->n = value;
 	new_node->parent = parent;
-	new_node->right = NULL;
+	new_node->right = parent->right;
+	parent->right = new_node;
+	new_node->left = NULL;
 
-	if (parent->left != NULL)
-	{
-		reset = parent->left;
-		parent->left = new_node;
-		new_node->left = reset;
-	}
-	else
-		new_node->left = NULL;
+	if (new_node->right)
+		new_node->right->parent = new_node;
 
 	return (new_node);
 }

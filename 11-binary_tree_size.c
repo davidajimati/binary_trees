6b23f251c727a07ee4397934468cbce45253e897
davidajimati <ajimatidavid@gmail.com>
2023-03-30 21:50:04 +0100

    binary tree size function updated

--- 11-binary_tree_size.c
+++ 11-binary_tree_size.c
@@ -9,37 +9,12 @@
 
 size_t binary_tree_size(const binary_tree_t *tree)
 {
-	size_t depth, ans = 2, repl;
-
-	depth = binary_tree_depth(tree);
-
-	repl = depth;
-	while (repl > 0)
-	{
-		ans *= depth;
-		repl -= 1;
-	}
-	return (0);
-}
-
-
-/**
- * binary_tree_depth -  measures the depth of a node in a binary tree
- * @tree: pointer to the node to measure the depth
- * Return: depth of tree or 0 if tree is NULL
-*/
-
-size_t binary_tree_depth(const binary_tree_t *tree)
-{
-	size_t depth = 0;
+	size_t size;
 
 	if (tree)
 	{
-		if (tree->parent)
-			depth = 1 + binary_tree_depth(tree->parent);
-
-		return (depth);
-
+		size = 1 + binary_tree_size(tree->left) + binary_tree_size(tree->right);
+		return (size);
 	}
 	return (0);
 }

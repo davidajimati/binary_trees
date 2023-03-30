656d782070f4995c132a88decb4f3f72a4889d12
davidajimati <ajimatidavid@gmail.com>
2023-03-30 17:27:42 +0100

    binary tree hieght | solved with recursion

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -1,24 +1,27 @@
 #include "binary_trees.h"
-#include <limits.h>
-#include <math.h>
 
+size_t max(size_t one, size_t two);
 /**
  * binary_tree_height - finds height of a tree
  * @tree: pointer to root node
  * Return: maximum depth(height) or 0 if tree is NULL
 */
+
 size_t binary_tree_height(const binary_tree_t *tree)
 {
-    size_t height = 0;
-    size_t left_wing;
-    size_t right_wing;
+	size_t left = 0, right = 0;
 
-    if (!tree)
-        return (0);
+	if (tree)
+	{
+		if (tree->left)
+			left = 1 + binary_tree_height(tree->left);
 
-    height += 1;
+		if (tree->right)
+			right = 1 + binary_tree_height(tree->right);
 
-    left_wing = binary_tree_height(tree->left);
-    right_wing = binary_tree_height(tree->right);
-    return (max(left_wing, right_wing));
+		if (left >= right)
+			return (left);
+		return (right);
+	}
+	return (0);
 }

e2823ac8065dafa4e8ed078e56aa760a1a9652fe
davidajimati <ajimatidavid@gmail.com>
2023-04-03 15:19:53 +0100

    updates

--- 15-binary_tree_is_full.c
+++ 15-binary_tree_is_full.c
@@ -4,37 +4,24 @@ int confirm(const binary_tree_t *node);
 /**
  * binary_tree_is_full - checks if a binary tree is full
  * @tree: pointer to the target tree
+ * Description ; it's true if the last node is a leaf node
+ * and so, we'll have to check for leaf nodes.
  * Return: 1 if tree is full, 0 otherwise. 0 if tree is NULL
  */
 
 int binary_tree_is_full(const binary_tree_t *tree)
 {
-	int status, i = 0;
-
-	status = (confirm(tree));
-	if (status == 1 && tree)
-	{
-		status = binary_tree_is_full(tree->left);
-		status = binary_tree_is_full(tree->right);
-	}
-	i = 1;
-	if (i == 1 && tree->parent && (!tree->left && !tree->right))
-		return (1);
-
-	return (status);
-}
-
-/**
- * confirm - checks if a node is full
- * @node: pointer to the target
- * Return: 0 = No, 1 = Yes
- */
-int confirm(const binary_tree_t *node)
-{
-	if (!node)
+	/* if no node */
+	if (!tree)
 		return (0);
 
-	if (node->left && node->right)
+	/* If it's a leaf node */
+	if (((!tree->left) && (!tree->right)) && tree->parent)
 		return (1);
+
+	if (tree->right && tree->left)
+	{
+		return (binary_tree_is_full(tree->left)) && binary_tree_is_full(tree->right);
+	}
 	return (0);
 }

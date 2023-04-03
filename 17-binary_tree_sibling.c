3d4f8b3e0b8ddc6f433e9398469dd3b03139f4ab
davidajimati <ajimatidavid@gmail.com>
2023-04-03 23:16:52 +0100

    file created | boilerplate written

---
+++ 17-binary_tree_sibling.c
@@ -0,0 +1,14 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_sibling - finds the sibling of a node
+ * @node: pointer to the node whose sibling we must find
+ * Return: A pointer to the sibling node
+ *			NULL if node or Parent is NULL
+ *			NULL is node has no sibling
+*/
+
+binary_tree_t *binary_tree_sibling(binary_tree_t *node)
+{
+
+}

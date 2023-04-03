1bc1285232b1666345de09158c792328ab820f75
davidajimati <ajimatidavid@gmail.com>
2023-04-03 15:30:56 +0100

    function -> Check if binary tree is perfect

---
+++ 16-binary_tree_is_perfect.c
@@ -0,0 +1,11 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_is_perfect - checks if a binary tree is perfect
+ * @tree: pointer to target tree
+ * Return: 0 if tree is NULL. 1 if tree is perfect, 0 otherwise
+ */
+
+int binary_tree_is_perfect(const binary_tree_t *tree)
+{
+}

761597929dc0c05b66229e155ef8bc869bcc300d
davidajimati <ajimatidavid@gmail.com>
2023-04-03 13:27:16 +0100

    function to check if binary tree is full -> in Process

---
+++ 15-binary_tree_is_full.c
@@ -0,0 +1,40 @@
+#include "binary_trees.h"
+int confirm(const binary_tree_t *node);
+
+/**
+ * binary_tree_is_full - checks if a binary tree is full
+ * @tree: pointer to the target tree
+ * Return: 1 if tree is full, 0 otherwise. 0 if tree is NULL
+ */
+
+int binary_tree_is_full(const binary_tree_t *tree)
+{
+	int status;
+
+	status = (confirm(tree));
+	if (tree->parent && (!tree->left && !tree->right))
+		return (status);
+
+
+	if (status == 1 && tree)
+	{
+		status = binary_tree_is_full(tree->left);
+		status = binary_tree_is_full(tree->right);
+	}
+	return (status);
+}
+
+/**
+ * confirm - checks if a node is full
+ * @node: pointer to the target
+ * Return: 0 = No, 1 = Yes
+ */
+int confirm(const binary_tree_t *node)
+{
+	if (!node)
+		return (0);
+
+	if (node->left && node->right)
+		return (1);
+	return (0);
+}

a72d01dbbcfa4fd324fa53d5531b3b8344a5ad64
davidajimati <ajimatidavid@gmail.com>
2023-04-03 23:09:18 +0100

    style checked

--- 16-binary_tree_is_perfect.c
+++ 16-binary_tree_is_perfect.c
@@ -81,9 +81,9 @@ size_t binary_tree_height(const binary_tree_t *tree)
 
 	if (tree)
 	{
-		left = tree->left ? 1 + binary_tree_height(tree->left): 0;
-		right = tree->right ? 1 + binary_tree_height(tree->right): 0;
-		return left > right ? left : right;
+		left = tree->left ? 1 + binary_tree_height(tree->left) : 0;
+		right = tree->right ? 1 + binary_tree_height(tree->right) : 0;
+		return (left > right ? left : right);
 	}
 	return (0);
 }
